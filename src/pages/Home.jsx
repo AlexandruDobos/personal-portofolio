6544abf0b728e9f6f2eea03aaef009e4b97722a6
alexandrudobos <dobosalex77@yahoo.com>
2025-01-22 16:48:59 +0200

    Fix import errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
       <section className="text-center py-20 bg-gray-100">
-        <h1 className="text-4xl font-bold">Salut! Eu sunt [Numele Tău]</h1>
+        <h1 className="text-4xl font-bold">Salut! Eu sunt Alex</h1>
         <p className="mt-4 text-lg text-gray-600">Dezvoltator Web | React Enthusiast | Freelancer</p>
         <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded">Explorează portofoliul</button>
       </section>
