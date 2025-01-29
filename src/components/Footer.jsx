0441668666910ceaa8c36bdf0a5930de302cef4c
alexandrudobos <dobosalex77@yahoo.com>
2025-01-29 16:22:22 +0200

    Modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ function Footer() {
     <footer className="bg-gray-800 text-white py-4 text-center">
       <p>© 2025 My Portfolio. All rights reserved.</p>
       <div className="flex justify-center space-x-4 mt-2">
-        <a href="https://github.com/" className="hover:text-gray-400">GitHub</a>
-        <a href="https://linkedin.com/" className="hover:text-gray-400">LinkedIn</a>
-        <a href="mailto:your_email@gmail.com" className="hover:text-gray-400">Email</a>
+        <a href="https://github.com/AlexandruDobos" className="hover:text-gray-400">GitHub</a>
+        <a href="https://www.linkedin.com/in/alexdobosh/" className="hover:text-gray-400">LinkedIn</a>
+        <a href="mailto:dobosalexandru2502@gmail.com" className="hover:text-gray-400">Email</a>
       </div>
     </footer>
   );
